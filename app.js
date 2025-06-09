fcc22992f8f403e6f481f51bb757e060619c2aa1
Freeredbull <mrstelios@outlook.com>
2025-06-09 09:51:23 +0300

    Update app.js
    import { createClient } from 'https://esm.sh/@supabase/supabase-js';

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
 const SUPABASE_URL = "https://gycoadvqrogvmrdmxntn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5Y29hZHZxcm9ndm1yZG14bnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMDc2MzcsImV4cCI6MjA2NDc4MzYzN30.hF_0bAwBs1kcCxuSL8UypC2SomDtuCXSVudXSDhwOpI";
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
